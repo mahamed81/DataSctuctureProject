c13b608163682bef7663e006750a0cb70829634e
Eddy Flores <Eddy Flores@141.224.201.156>
2016-11-02 13:39:56 -0500

    Statrted Link List for Group

--- DataSctructureProject/src/GameController.java
+++ DataSctructureProject/src/GameController.java
@@ -1,35 +1,12 @@
+import java.util.Scanner;
+public class GameController{
+	static Scanner scan = new Scanner(System.in);//new scanner
+	public static void main(String[] args) {
+		System.out.println("how many players would you like to make?");
+		int numberOfPlayers = scan.nextInt();
+		group groupA= new group(numberOfPlayers);//group will create a link list of class type Players.
+	
 
-public class GameController {
-/*
- 
- 
- *pass "how many players"
- |
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- 
- */
-	
-	
+	}
+
 }
