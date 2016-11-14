ad2fc0aab1d7cb86d0d5e46660e8782844a652ce
mahamed81 <adenh@augsburg.edu>
2016-11-13 20:17:02 -0600

    Update GameController.java

--- DataSctructureProject/src/GameController.java
+++ DataSctructureProject/src/GameController.java
@@ -4,8 +4,17 @@ public class GameController{
 	public static void main(String[] args) {
 		System.out.println("how many players would you like to make?");
 		int numberOfPlayers = scan.nextInt();
-		group groupA= new group(numberOfPlayers);//group will create a link list of class type Players.
-	
+		//group groupA= new group(numberOfPlayers);//group will create a link list of class type Players.
+		
+		puesudocode:
+		
+		while (player1 holds a card){
+		player1 asks a player("Do you have" the card )
+		if ( a player has any of the card)
+		the player gives all the cards to player1	
+			}	
+		break/// this is when the player that was asked a card does not have it 
+			
 
 	}
 
